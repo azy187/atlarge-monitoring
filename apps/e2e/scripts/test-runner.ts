d002b7a329c7a6c18046e1caa4542b20a8c7d0a1
azy187 <nathanadesile@gmail.com>
2025-06-18 22:45:25 +0100

    v23

--- apps/e2e/scripts/test-runner.ts
+++ apps/e2e/scripts/test-runner.ts
@@ -1,13 +1,15 @@
-import { execSync } from "child_process";
+import { spawn } from "child_process";
 import _test_parse from "./report-parser";
 
 function main() {
   console.log("0. starting 'test-runner.ts'");
   console.log("1. calling 'npx playwright test'");
-  const result = execSync("npx playwright test");
-  console.log(`result:\n${result}`);
-  console.log("2. calling _test_parse() from 'report-parser.ts'");
-  _test_parse();
+
+  const testProcess = spawn("npx playwright test");
+  testProcess.on("exit", () => {
+    console.log("2. calling _test_parse() from 'report-parser.ts'");
+    _test_parse();
+  });
 }
 
 main();
