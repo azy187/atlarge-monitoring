8e47f11dfaba155b48f5f9475f7ec3248832bfe3
azy187 <nathanadesile@gmail.com>
2025-06-18 22:46:35 +0100

    v24

--- apps/e2e/scripts/test-runner.ts
+++ apps/e2e/scripts/test-runner.ts
@@ -5,7 +5,7 @@ function main() {
   console.log("0. starting 'test-runner.ts'");
   console.log("1. calling 'npx playwright test'");
 
-  const testProcess = spawn("npx playwright test");
+  const testProcess = spawn("npx", ["playwright test"]);
   testProcess.on("exit", () => {
     console.log("2. calling _test_parse() from 'report-parser.ts'");
     _test_parse();
