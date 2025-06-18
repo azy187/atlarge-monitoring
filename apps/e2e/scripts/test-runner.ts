50ea32a755acf92feb9a9892dc5bec83a1427ebf
azy187 <nathanadesile@gmail.com>
2025-06-18 20:54:35 +0100

    test-runner v2

--- apps/e2e/scripts/test-runner.ts
+++ apps/e2e/scripts/test-runner.ts
@@ -1,11 +1,10 @@
-import { spawnSync } from "child_process";
+import { execSync } from "child_process";
 
 function main() {
   console.log("started 'test-runner.ts'");
-  const result = spawnSync("npx playwright test");
+  const result = execSync("npx playwright test");
 
-  console.log(`result.output:\n${result.status}`);
-  console.log(`result.status:\n${result.output}`);
+  console.log(`result:\n${result}`);
 }
 
 main();
